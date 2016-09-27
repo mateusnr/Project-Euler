c85d9d8cd33b574d8e3d1ebb8cd6ee4f9005a44c
Mateus Nóbrega <mateusn@outlook.com>
2016-09-27 08:37:48 -0300

    Added problem-1 (d)

--- Problem1/C/problem-1.c
+++ Problem1/C/problem-1.c
@@ -8,10 +8,12 @@
 
 #include <stdio.h>
 
-int main(){
+int main()
+{
 	int i, total = 0;
 	
-	for (i = 0; i < 1000; i++){
+	for (i = 0; i < 1000; i++)
+	{
 		if (i % 3 == 0 || i % 5 == 0)
 			total += i;
 	}
